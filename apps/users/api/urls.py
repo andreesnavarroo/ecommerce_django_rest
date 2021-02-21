9b9bbd6cbe20cca99f260d78aaa6653bed62206c
andreesnavarroo <andreesnavarroo@gmail.com>
2021-02-21 15:38:42 -0500

    Registro con @API_VIEW y SERIALIZER

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from apps.users.api.api import UserAPiView
+from apps.users.api.api import user_api_view
 
 urlpatterns = [
-    path('usuario/', UserAPiView.as_view(), name = 'usuario_api')
+    path('usuario/', user_api_view, name = 'usuario_api')
 ]
