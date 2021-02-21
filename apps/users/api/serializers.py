0df67ae6d8adf7ad55164916fe3fd3ac040c5f23
andreesnavarroo <andreesnavarroo@gmail.com>
2021-02-21 17:39:03 -0500

    SERIALIZER Orden en q se ejecutan las validaciones

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -5,3 +5,19 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class TestUserSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length = 200)
+    email = serializers.EmailField()
+
+    def validate_name(self,value):
+        print(value)
+        return value
+
+    def validate_email(self,value):
+        print(value)
+        return value
+
+    def validate(self,data):
+        print("general validacion")
+        return data
